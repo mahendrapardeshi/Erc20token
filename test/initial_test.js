60d3f86b9c3a23932aeaa657f9429a28196779d9
Mahendra <mahendrapardeshi2001@gmail.com>
2022-12-18 16:39:12 +0530

    Added new test cases

--- test/initial_test.js
+++ test/initial_test.js
@@ -35,6 +35,60 @@ describe("Erc20 smart contract testing", function () {
     await wallet.buy(option);
     expect(await wallet.balanceOf(accounts[2].address)).to.equal(calculate);
   });
+
+  it("you dont have permission to withdraw ether from contract", async function () {
+    const wallet = token.connect(accounts[2]);
+    await expect(wallet.withdraw(amount)).to.be.reverted;
+  });
+
+  it("transfer amount to destination account", async function () {
+    await token.transfer(accounts[1].address, amount);
+    expect(await token.balanceOf(accounts[1].address)).to.equal(amount);
+  });
+
+  it("Can not transfer above the amount", async function () {
+    const wallet = token.connect(accounts[3]);
+    await expect(wallet.transfer(accounts[1].address, 1)).to.be.reverted;
+  });
+
+  it("Do not transfer from an empty account", async () => {
+    const wallet = token.connect(accounts[3]);
+    await expect(wallet.transfer(accounts[1].address, 1)).to.be.reverted;
+  });
+
+  it("Test Minting token", async () => {
+    const before_mint = await token.balanceOf(accounts[0].address);
+    await token.mint(accounts[0].address, amount);
+    const after_mint = await token.balanceOf(accounts[0].address);
+    expect(after_mint).to.equal(before_mint.add(amount));
+  });
+
+  it("Test burn token", async () => {
+    const before_burn = await token.balanceOf(accounts[0].address);
+    await token.burn(accounts[0].address, amount);
+    const after_burn = await token.balanceOf(accounts[0].address);
+    expect(after_burn).to.equal(before_burn.sub(amount));
+  });
+
+  it("Test Withdraw token", async function () {
+    const before_withdraw = await accounts[0].getBalance();
+    await token.withdraw(amount);
+    const after_withdraw = await accounts[0].getBalance();
+    expect(before_withdraw.lt(after_withdraw)).to.equal(true);
+  });
+
+  it("Don't have enough ehter to buy token", async () => {
+    const wallet = token.connect(accounts[3]);
+    const big_amount = ethers.utils.parseEther("9999");
+    const option = { value: big_amount };
+    let error;
+    try {
+      await wallet.buy(option);
+    } catch (err) {
+      error = "Sender does not have enough funds";
+    }
+    expect(error).to.equal("Sender does not have enough funds");
+  });
 });
 
 // account 2 - is empty in the begining
